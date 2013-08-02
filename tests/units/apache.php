7400a09587db36bf2faefa30c0edb7d9767397eb
a.cianfarani <a.cianfarani@c2is.fr>
2013-08-02 09:44:59 +0200

    Test new atoum config

--- tests/units/apache.php
+++ tests/units/apache.php
@@ -10,7 +10,7 @@ $p = getcwd();
 file_put_contents("/tmp/andre.log",$p);
 require_once 'vendor/bin/atoum';
 
-include './qsdchecklampconf.php';
+include './checklampconf.php';
 
 use \mageekguy\atoum;
 
